a300173c1411e209ceba7825df875f59718b64a6
Erinn Neidinger <erinndneidinger@gmail.com>
2019-12-11 16:32:41 -0600

    Completed the add method for the binary search tree. Started to code the search method.

---
+++ BinarySearchTree/BinarySearchTree/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BinaryTree binaryTree = new BinaryTree();
+            binaryTree.Add(5);
+            binaryTree.Add(4);
+            binaryTree.Add(7);
+            binaryTree.Add(6);
+            binaryTree.Add(1);
+            binaryTree.Add(15);
+
+            binaryTree.Find(7);
+
+        }
+    }
+}
